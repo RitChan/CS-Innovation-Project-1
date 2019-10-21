f04fe0c5ca899105efdf6c6faec8e84bbad1f3b5
Rit <ritian_master@icloud.com>
2019-10-21 19:39:36 +0800

    增加注解和图片精度

--- pro/statistic.py
+++ pro/statistic.py
@@ -12,7 +12,8 @@ from myio import IO
 
 
 def main():
-    table_statistics_hist(8, norm=True)
+    n = 8
+    # table_statistics_hist(n, norm=False)
 
 
 def table_gc_distribution(n, unit):
@@ -79,6 +80,7 @@ def table_statistics_hist(n, norm=False):
         gcs, bins=10, range=(0, 1), density=norm, 
         facecolor='#ffcccc', edgecolor='black'
     )
+    plt.xticks([0.1*i for i in range(11)])
 
     # set text
     plt.xlabel('gc-content')
@@ -87,6 +89,32 @@ def table_statistics_hist(n, norm=False):
     else:
         plt.ylabel('density')
     plt.title(f'Histogram of gc-content in a table with n={n}')
+
+    # set annotation
+    if n == 5:
+        # (0.4, 5), (0.6, 5), (0.2, 0)
+        plt.annotate(r'$gc = 0.2$', (0.15, 1000))
+        plt.annotate(r'$gc = 0.4$', (0.30, 16500))
+        plt.annotate(r'$gc = 0.6$', (0.61, 16000))
+    elif n == 6:
+        # 0.333, 0.5, 0.667
+        plt.annotate(r'$gc \approx 0.333$', (0.3, 3000))
+        plt.annotate(r'$gc = 0.5$', (0.5, 81000))
+        plt.annotate(r'$gc \approx 0.667$', (0.62, 51000))
+    elif n == 7:
+        # 0.2, 0.4, 0.6
+        plt.annotate(r'$\frac{2}{7} \approx 0.2857$', (0.2, 20000))
+        plt.annotate(r'$\frac{3}{7} \approx 0.4285$', (0.36, 250000))
+        plt.annotate(r'$\frac{4}{7} \approx 0.5714$', (0.62, 270000))
+        plt.annotate(r'$\frac{5}{7} \approx 0.7142$', (0.7, 20000))
+    elif n == 8:
+        # 0.3, 0.4, 0.6
+        plt.annotate(r'$gc = 0.375$', (0.3, 560000))
+        plt.annotate(r'$gc = 0.5$', (0.35, 1000000))
+        plt.annotate(r'$gc = 0.625$', (0.62, 560000))
+
+    # save
+    plt.savefig(f'../pic/gc_distri_hist{n}.png', format='png', dpi=300)
 
     # show
     plt.show()
